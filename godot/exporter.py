13916d5dfa53c706af077b583858e979cfb22118
Ahmad Rifai <riff.blend@gmail.com>
2024-09-11 17:34:20 +0700

    add heightmap

--- godot/exporter.py
+++ godot/exporter.py
@@ -26,42 +26,85 @@ class ExportShader(Operator):
     script_template = '''
 shader_type spatial;
 
-uniform float normal_depth = 1.0;
+uniform int heightmap_min_layers : hint_range(1, 64) = 32;
+uniform int heightmap_max_layers : hint_range(1, 64) = 32;
 {0}vec4 layer(vec4 foreground, vec4 background) {{
     return foreground * foreground.a + background * (1.0 - foreground.a);
 }}
 
-void fragment() {{ {1}
+vec2 setHeight(vec2 uv, vec3 view_dir, sampler2D heightmap, float scale){{
+	vec2 base_uv = uv;
+
+	// Height Deep Parallax: Enabled
+	float num_layers = mix(float(heightmap_max_layers), float(heightmap_min_layers), abs(dot(vec3(0.0, 0.0, 1.0), view_dir)));
+	float layer_depth = 1.0 / num_layers;
+	float current_layer_depth = 0.0;
+	vec2 p = view_dir.xy * scale * 0.01;
+	vec2 delta = p / num_layers;
+	vec2 ofs = base_uv;
+	float depth = 1.0 - texture(heightmap, ofs).r;
+
+	float current_depth = 0.0;
+	while (current_depth < depth) {{
+		ofs -= delta;
+		depth = 1.0 - texture(heightmap, ofs).r;
+
+		current_depth += layer_depth;
+	}}
+
+	vec2 prev_ofs = ofs + delta;
+	float after_depth = depth - current_depth;
+	float before_depth = (1.0 - texture(heightmap, prev_ofs).r) - current_depth + layer_depth;
+
+	float weight = after_depth / (after_depth - before_depth);
+	ofs = mix(ofs, prev_ofs, weight);
+	base_uv = ofs;
+
+	return base_uv;
+}}
+
+void fragment() {{ 
+    vec3 view_dir = normalize(normalize(-VERTEX + EYE_OFFSET) * mat3(TANGENT, -BINORMAL, NORMAL));{1}
     ALBEDO = albedo_all.rgb;
 }}
 
 '''
 
     script_vars = '''
-uniform sampler2D {0};
+uniform sampler2D {0}:source_color, filter_linear_mipmap, repeat_enable;
 uniform vec2 {1} = vec2({2},{3}); 
 '''
-    script_vars_roughness = "uniform sampler2D {}_roughness;\n"
+    script_vars_roughness = "uniform sampler2D {}_roughness:hint_roughness_r, filter_linear_mipmap, repeat_enable;\n"
 
-    script_vars_normal = "uniform sampler2D {}_normal;\n"
+    script_vars_normal = '''uniform sampler2D {0}_normal:hint_roughness_normal, filter_linear_mipmap, repeat_enable;
+uniform float {0}_normal_depth = 1.0;
+'''
+
+    script_vars_heightmap = "uniform sampler2D {0}_heightmap:hint_default_black, filter_linear_mipmap, repeat_enable;\nuniform float {0}_heightmap_scale = 5.0;\n"
 
     script_mask_vars = "uniform sampler2D {0};"
     
+    script_fragment_base_uv_heightmap = '''
+    base_uv_{0} = setHeight(UV, view_dir, {1}_heightmap, {1}_heightmap_scale);'''
+
     script_fragment_var = '''
 
-    vec2 scaled_uv_{0} = UV * {1};
+    vec2 base_uv_{0} = UV;{3}
+    vec2 scaled_uv_{0} = base_uv_{0} * {1};
     vec4 albedo_{0} = texture({2}, scaled_uv_{0});'''
 
     script_fragment_roughness_var = '''
     vec4 roughness_{0} = texture({1}, scaled_uv_{0});'''
     script_fragment_normal_var = '''
-    vec4 normal_{0} = texture({1}, scaled_uv_{0});'''
+    vec4 normal_{0} = texture({1}, scaled_uv_{0});
+    vec4 normal_depth_{0} = vec4({1}_depth, 0, 0, 1);'''
 
     script_mask_fragment_var = '''
-    vec4 mask_{0} = texture({1}, UV);
+    vec4 mask_{0} = texture({1}, base_uv_{0});
     albedo_{0}.a = mask_{0}.r;''' 
     script_mask_normal_var = '''
-    normal_{0}.a = mask_{0}.r;'''
+    normal_{0}.a = mask_{0}.r;
+    normal_depth_{0}.a = mask_{0}.r;'''
     script_mask_roughness_var = '''
     roughness_{0}.a = mask_{0}.r;'''
 
@@ -91,17 +134,20 @@ uniform vec2 {1} = vec2({2},{3});
 '''
 
     script_normal_1 = '''
-    vec4 normal_all = normal_{};
+    vec4 normal_all = normal_{0};
+    vec4 normal_depth_all = normal_depth_{0};
 '''
     script_normal_combine_0 = '''
-    vec4 normal_all = layer(normal_{0}, normal_{1});'''
+    vec4 normal_all = layer(normal_{0}, normal_{1});
+    vec4 normal_depth_all = layer(normal_depth_{0}, normal_depth_{1});'''
 
     script_normal_combine_next = '''
     normal_all = layer(normal_all, normal_{0});
-'''
+    normal_depth_all = layer(normal_depth_all, normal_depth_{0});'''
+
     script_normal_fragment = '''
     NORMAL_MAP = normal_all.rgb;
-    NORMAL_MAP_DEPTH = normal_depth;
+    NORMAL_MAP_DEPTH = normal_depth_all.r;
 '''
   #vec4 albedo = texture({1}, scaled_uv_{0});
 
@@ -201,7 +247,9 @@ uniform vec2 {1} = vec2({2},{3});
 
                 skala = mapping.inputs[3].default_value
                 global_vars += self.script_vars.format(layer_var, scale_var, skala.x, skala.y)
-                fragment_vars += self.script_fragment_var.format(index, scale_var, layer_var)
+
+                heightmap_uv_script = ""
+                fragment_var = ""
 
                 source = get_layer_source(layer)
 
@@ -219,8 +267,8 @@ uniform vec2 {1} = vec2({2},{3});
                 channel:Layer.YLayerChannel
                 for id_ch, channel in enumerate(layer.channels):
                     ch_name = yp.channels[id_ch].name
-                    print("channel ", channel.name, " enable ", channel.enable, " name_", yp.channels[id_ch].name)
                     if channel.enable:
+                        print("channel ", channel.name, " name_", yp.channels[id_ch].name)
                         ch_image_path = ""
                         ch_image_path_1 = ""
 
@@ -228,7 +276,7 @@ uniform vec2 {1} = vec2({2},{3});
                             source_ch = get_channel_source(channel, layer)
                             ch_image_path = source_ch.image.filepath_from_user()
                     
-                            print("channel path ", id_ch, " = ",ch_image_path)
+                            print("channel path 0", id_ch, " = ",ch_image_path)
 
                         if channel.override_1:
                             source_ch_1 = get_channel_source_1(channel, layer)
@@ -248,17 +296,26 @@ uniform vec2 {1} = vec2({2},{3});
                             layer_roughness = layer_var + "_roughness"
                             asset_args.append(layer_roughness)
                             asset_args.append(bpy.path.basename(ch_image_path))
-                            fragment_vars += self.script_fragment_roughness_var.format(index, layer_roughness)
+                            fragment_var += self.script_fragment_roughness_var.format(index, layer_roughness)
                             
                         elif ch_name == "Normal":
                             global_vars += self.script_vars_normal.format(layer_var)
+                            global_vars += self.script_vars_heightmap.format(layer_var)
                             normal_overrides.append(layer_idx)
+
+                            layer_heightmap = layer_var + "_heightmap"
+
+                            heightmap_uv_script = self.script_fragment_base_uv_heightmap.format(index, layer_var)
+                            asset_args.append(layer_heightmap)
+                            asset_args.append(bpy.path.basename(ch_image_path)) # todo : override 0 or override 1
 
                             layer_normal = layer_var + "_normal"
                             asset_args.append(layer_normal)
                             asset_args.append(bpy.path.basename(ch_image_path_1)) # todo : override 0 or override 1
-                            fragment_vars += self.script_fragment_normal_var.format(index, layer_normal)
-
+                            fragment_var += self.script_fragment_normal_var.format(index, layer_normal)
+                
+                fragment_var = self.script_fragment_var.format(index, scale_var, layer_var, heightmap_uv_script) + fragment_var
+                fragment_vars += fragment_var
 
                 # print("filepath ", index, " = ",source.image.filepath_from_user())
                 # print("rawpath ", index, " = ",source.image.filepath)
